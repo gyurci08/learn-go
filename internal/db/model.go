ed90984ce3526db26ef7e41375bb179fda60b570
gyurci08 <jandzso.gyorgy@webstar.hu>
2025-07-15 09:07:41 +0200

    rename database manually

--- internal/db/model.go
+++ internal/db/model.go
@@ -1,6 +1,12 @@
 package db
 
+// Define the HelloWorld model
 type HelloWorld struct {
 	ID      uint   `gorm:"primaryKey" json:"id"`
 	Message string `gorm:"type:varchar(255)" json:"message"`
+}
+
+// Rename the table
+func (HelloWorld) TableName() string {
+	return "hello_worlds"
 }
